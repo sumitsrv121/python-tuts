78b82a25d0b9da6f6c62f95d1455a546342b1a3f
sumit <sumitsaurav121@gmail.com>
2025-12-22 17:14:18 +0530

    numpy array practice

---
+++ numpy_array.py
@@ -0,0 +1,22 @@
+from numpy import array, linspace, arange, logspace, zeros, ones
+
+# arr = array([1, 2, 3, 4, 5.0], dtype=float)
+#
+# print(arr)
+
+# arr = linspace(1, 10)
+# print(arr)
+
+# arr = arange(1, 11, 2)
+# print(arr)
+
+
+# arr = logspace(1, 40, 5)
+# print(arr)
+
+
+zeros_arr = zeros((3,4))
+print(zeros_arr)
+
+ones_arr = ones((3,4))
+print(ones_arr)
