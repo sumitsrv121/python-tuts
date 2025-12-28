d3fa4065be3540a0cf6281e8ec757558a335d392
sumit <sumitsaurav121@gmail.com>
2025-12-28 12:33:45 +0530

    insert query

--- db_connection.py
+++ db_connection.py
@@ -4,12 +4,22 @@ mydb = connect(host="localhost", user="", passwd="")
 
 cursor = mydb.cursor()
 
-cursor.execute("SHOW DATABASES")
-
-rows = cursor.fetchall()
-
-# for row in rows:
+# cursor.execute("SHOW DATABASES")
+#
+# rows = cursor.fetchall()
+#
+# # for row in rows:
+# #     print(row)
+# cursor.execute("select * from telusko.student")
+# fetchall = cursor.fetchall()
+# for row in fetchall:
 #     print(row)
+
+
+cursor.execute("INSERT INTO telusko.student value(%s,%s)", ("sumit", "Heritage"))
+
+mydb.commit()
+
 cursor.execute("select * from telusko.student")
 fetchall = cursor.fetchall()
 for row in fetchall:
